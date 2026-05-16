ea3b1258d3de97813ae9815ce99beb85706832cd
Lennoxgonz <75677560+Lennoxgonz@users.noreply.github.com>
2026-05-16 16:31:59 -0400

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        int[] res = [];
+        Dictionary<int, int> map = [];
+        for (int i = 0; i < nums.Length; i++) {
+            int compliment = target - nums[i];
+            if (map.TryGetValue(compliment, out int value)) {
+                return [value, i];
+            }
+            map[nums[i]] = i;
+        }
+        return res;
+    }
+}
