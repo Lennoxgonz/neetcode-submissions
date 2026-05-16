b44523e9b5b32e7b98aeb668e811b3f4f704e0e5
Lennoxgonz <75677560+Lennoxgonz@users.noreply.github.com>
2026-05-16 19:06:13 -0400

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public bool SearchMatrix(int[][] matrix, int target) {
+        (int rows, int cols) = (matrix.Length, matrix[0].Length);
+        (int t, int b) = (0, rows - 1);
+        while (t <= b)
+        {
+            int m = (t + b) / 2;
+            if (target > matrix[m][cols - 1]) t = m + 1;
+            else if (target < matrix[m][0]) b = m - 1;
+            else break;
+        }
+
+        ReadOnlySpan<int> row = matrix[(t + b) / 2];
+        (int l, int r) = (0, cols - 1);
+        while (l <= r)
+        {
+            int m = (l + r) / 2;
+            if (target > row[m]) l = m + 1;
+            else if (target < row[m]) r = m - 1;
+            else return true;
+        }
+        return false;
+    }
+}
